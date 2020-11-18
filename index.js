38dcedd350fd61715656992fc62eaf1c5cc85ebb
wynterwolf <shinyguns@gmail.com>
2020-11-18 12:18:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// remove ID Main (without knowing parent)
+let node = document.getElementById("main");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
+
+// add header
+let newHeader = document.createElement('h1');
+// set attribute to victory
+newHeader.setAttribute("id", "victory");
+// set inner html
+newHeader.innerHTML = "Samantha is the champion";
+// append newHeader to body
+document.body.appendChild(newHeader)
