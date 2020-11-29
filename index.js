2f1366ed8844c79f4ec475ec612d84155c5bf3c2
rumikoacopa <rumikorios@gmail.com>
2020-11-28 19:37:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory'); //type of attr and name in argument
+newHeader.innerHTML = "Rumiko is the champion"  //whatever you put inside will be only in innerHTML
+
+
