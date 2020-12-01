50fcc5724acd300d2a8c2ad1d57b700aac67459e
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-12-01 00:06:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+document.querySelector("main").remove();
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Dmitriy is the champion"
+
+
+
+
+
+
+
+// let element = document.createElement("div"); //Assignment of div
+// document.body.appendChild(element);  //Adding the element to the body
+
+// let ul = document.createElement("ul");  //Assignment of ul
+
+// for (let i = 0; i < 3; i++) { 
+//   let li = document.createElement("li");  //Assignment of li
+//   li.innerHTML = (i + 1).toString(); //Adding text to innerHTML of li
+//   ul.appendChild(li); //Adding the li to ul 
+// }
+
+// element.appendChild(ul);  //Returning 
+
+
+// let element = document.querySelector("p#greeting");
+// element.innerHTML = "Hello, DOM!"; 
