b3a26e8287b4c08b705377b38f918c783fc97132
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-06 22:26:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'YOUR-NAME is the champion';
